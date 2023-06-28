e5f5f61fe6d8982cfb1dadff43b9f5138dfda1fe
OST-Gh <108372693+OST-Gh@users.noreply.github.com>
2023-06-28 02:50:05 +0200

    chaining playlists is possible now

--- src/main.rs
+++ src/main.rs
@@ -29,65 +29,67 @@ fn main() {
 			.terminated(false),
 	]
 		.pipe(Handle::from);
-	let Some(path) = std::env::args().nth(1) else { return };
 
-	handle.print(format!("Loading and parsing data from [{path}]."));
-	let Playlist { song, name } = match fs::read_to_string(fmt_path(&path)).map(|contents| toml::from_str(&contents)) {
-		Ok(Ok(playlist)) => playlist,
-		Ok(Err(why)) => {
-			handle.print(format!("{LINE}A fatal error occured whilst attempting to parse the contents of [{path}]; '{ENBOLD}{why}{DISBOLD}'"));
-			return
-		},
-		Err(why) => {
-			handle.print(format!("{LINE}A fatal error occured whilst attempting to load the contents [{path}]; '{ENBOLD}{why}{DISBOLD}'"));
-			return
-		},
-	};
+	for path in std::env::args().skip(1) {
 
-	handle.print(format!("Loading all of the songs in [{name}]."));
-	let mut files: Vec<(Box<str>, File)> = song
-		.into_iter()
-		.filter_map(|Song { name, file }|
-			{
-				handle.print(format!("Loading the audio contents of [{name}]."));
-				match File::open(fmt_path(file)) {
-					Ok(contents) => Some((name, contents)),
-					Err(why) => {
-						handle.print(format!("{LINE}An error occured whilst attempting to load the audio contents of [{name}]; '{ENBOLD}{why}{DISBOLD}'"));
-						None
-					},
-				}
-			}
-		)
-		.collect();
-	handle.print('\0');
-
-	let mut generator = fastrand::Rng::new();
-
-	handle.print("Determining the output device.");
-	let handles = match rodio::OutputStream::try_default() {
-		Ok(handles) => handles,
-		Err(why) => {
-			handle.print(format!("{LINE}A fatal error occured whilst attempting to determine the default audio output device; '{ENBOLD}{why}{DISBOLD}'"));
-			return
-		},
-	};
-
-	handle.print(format!("Playing back all of the songs in [{name}]."));
-	while !files.is_empty() {
-		let (name, contents) = files.remove(generator.usize(0..files.len()));
-		match handles
-			.1
-			.play_once(contents)
-		{
-			Ok(playback) => {
-				handle.print(format!("Playing back the audio contents of [{name}]."));
-				playback.sleep_until_end();
-			},
-			Err(why) => {
-				handle.print(format!("{LINE}An error occured whilst attempting to playback [{name}] from the default audio output device; '{ENBOLD}{why}{DISBOLD}'"));
+		handle.print(format!("Loading and parsing data from [{path}]."));
+		let Playlist { song, name } = match fs::read_to_string(fmt_path(&path)).map(|contents| toml::from_str(&contents)) {
+			Ok(Ok(playlist)) => playlist,
+			Ok(Err(why)) => {
+				handle.print(format!("{LINE}An error occured whilst attempting to parse the contents of [{path}]; '{ENBOLD}{why}{DISBOLD}'"));
 				continue
 			},
+			Err(why) => {
+				handle.print(format!("{LINE}An error occured whilst attempting to load the contents [{path}]; '{ENBOLD}{why}{DISBOLD}'"));
+				continue
+			},
+		};
+
+		handle.print(format!("Loading all of the songs in [{name}]."));
+		let mut files: Vec<(Box<str>, File)> = song
+			.into_iter()
+			.filter_map(|Song { name, file }|
+				{
+					handle.print(format!("Loading the audio contents of [{name}]."));
+					match File::open(fmt_path(file)) {
+						Ok(contents) => Some((name, contents)),
+						Err(why) => {
+							handle.print(format!("{LINE}An error occured whilst attempting to load the audio contents of [{name}]; '{ENBOLD}{why}{DISBOLD}'"));
+							None
+						},
+					}
+				}
+			)
+			.collect();
+		handle.print('\0');
+
+		let mut generator = fastrand::Rng::new();
+
+		handle.print("Determining the output device.");
+		let handles = match rodio::OutputStream::try_default() {
+			Ok(handles) => handles,
+			Err(why) => {
+				handle.print(format!("{LINE}A fatal error occured whilst attempting to determine the default audio output device; '{ENBOLD}{why}{DISBOLD}'"));
+				return
+			},
+		};
+
+		handle.print(format!("Playing back all of the songs in [{name}]."));
+		while !files.is_empty() {
+			let (name, contents) = files.remove(generator.usize(0..files.len()));
+			match handles
+				.1
+				.play_once(contents)
+			{
+				Ok(playback) => {
+					handle.print(format!("Playing back the audio contents of [{name}]."));
+					playback.sleep_until_end();
+				},
+				Err(why) => {
+					handle.print(format!("{LINE}An error occured whilst attempting to playback [{name}] from the default audio output device; '{ENBOLD}{why}{DISBOLD}'"));
+					continue
+				},
+			}
 		}
 	}
 }
