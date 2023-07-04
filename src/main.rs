f4e6ac66a8a70715e3c1629c121fb2c7e20482be
OST-Gh <108372693+OST-Gh@users.noreply.github.com>
2023-07-04 04:40:15 +0200

    somewhat working?

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-use std::fs::{ self, File };
+use std::{
+	fs::{ self, File },
+	time::{ Duration, Instant },
+	thread::spawn,
+};
 use nitrogen::{ fmt_path, traits::* };
 use oxygen::*;
 use serde::Deserialize;
-use rodio::{
-	OutputStream,
-	Decoder,
-	source::{
-		Pausable,
-		Skippable,
-		Source,
+use rodio::OutputStream;
+use crossterm::{
+	terminal::{
+		enable_raw_mode,
+		disable_raw_mode,
+	},
+	event::{
+		self,
+		Event,
+		KeyEvent,
+		KeyCode,
 	},
 };
+use crossbeam_channel::{ unbounded, TryRecvError };
+use fastrand::Rng as Generator;
+use lofty::{ read_from_path, AudioFile };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 pub const LINE: &str = Formatting::UnderLined.enable();
 pub const ENBOLD: &str = Formatting::Bold.enable();
@@ -31,40 +42,16 @@ struct Song {
 
 struct Stream {
 	name: Box<str>,
-	file: Skippable<Pausable<Decoder<File>>>,
-}
-
-struct Streamlist {
-	name: Box<str>,
-	song: Vec<Stream>,
+	file: File,
+	time: Duration,
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-fn load(files: impl Iterator<Item = impl AsRef<str>>) -> Vec<Streamlist> {
-	let mut generator = fastrand::Rng::new();
-	files
-		.filter_map(|path|
-			{
-				let Songlist { name, mut song } = fs::read_to_string(fmt_path(path))
-					.ok()?
-					.pipe(|ref contents| toml::from_str(contents))
-					.ok()?;
-				let mut new = Vec::with_capacity(song.len());
-				while !song.is_empty() {
-					let song = song.swap_remove(generator.usize(0..song.len()));
-					let file = File::open(fmt_path(song.file))
-						.ok()?
-						.pipe(Decoder::new)
-						.ok()?
-						.pausable(false)
-						.skippable();
-					new.push(Stream { name: song.name, file })
-				}
-				Some(Streamlist { name, song: new })
-			}
-		)
-		.collect()
+enum Signal {
+	ManualExit, // signal sent by pb_ctl to main for indication of the user manually exiting
+	SkipPlayback,
+	TogglePlayback,
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 fn main() {
 	let handle = custom![
 		'\r',
@@ -74,6 +61,55 @@ fn main() {
 			.terminated(false),
 	]
 		.pipe(Handle::from);
+
+	handle.print(format!("Spinning up the playback control thread."));
+	let (sender, receiver) = unbounded();
+	let (exit_sender, exit_receiver) = unbounded();
+	let playback_control = spawn(
+		move || {
+			let handle = custom![
+				'\r',
+				Time::from(' '),
+				Colour::from(Empty)
+					.colour(colours::QING)
+					.terminated(false),
+			]
+				.pipe(Handle::from);
+			loop {
+				match exit_receiver.try_recv() {
+					Ok(_) => break,
+					Err(TryRecvError::Empty) => {
+						if let Err(why) = enable_raw_mode() { handle.print(format!("{LINE}An error occured whilst attempting to enable the raw mode of the current terminal; '{ENBOLD}{why}{DISBOLD}'")) };
+						let event = event::read();
+						if let Err(why) = disable_raw_mode() { handle.print(format!("{LINE}An error occured whilst attempting to disable the raw mode of the current terminal; '{ENBOLD}{why}{DISBOLD}'")) };
+						let send_result = match event {
+							Ok(Event::Key(KeyEvent { code: KeyCode::Char('q' | 'c'), .. })) => sender.send(Signal::ManualExit),
+							Ok(Event::Key(KeyEvent { code: KeyCode::Char(' ' | 'k'), .. })) => sender.send(Signal::TogglePlayback),
+							Ok(Event::Key(KeyEvent { code: KeyCode::Char('.' | 'l'), .. })) => sender.send(Signal::SkipPlayback),
+							Err(why) => {
+								handle.print(format!("{LINE}An error occured whilst attempting to read an event from the current terminal; '{ENBOLD}{why}{DISBOLD}'"));
+								continue
+							},
+							_ => continue,
+						};
+						if let Err(why) = send_result { handle.print(format!("{LINE}An error occured whilst attempting to send a signal to the playback; '{ENBOLD}{why}{DISBOLD}'")) };
+					},
+					Err(why) => handle.print(format!("{LINE}An error occured whilst attempting to receive a signal from the main thread; '{ENBOLD}{why}{DISBOLD}'")),
+				};
+			}
+		}
+	);
+
+	let mut generator = Generator::new();
+
+	handle.print("Determining the output device.");
+	let handles = match OutputStream::try_default() {
+		Ok(handles) => handles,
+		Err(why) => {
+			handle.print(format!("{LINE}A fatal error occured whilst attempting to determine the default audio output device; '{ENBOLD}{why}{DISBOLD}'"));
+			return
+		},
+	};
 
 	for path in std::env::args().skip(1) {
 
@@ -91,44 +127,62 @@ fn main() {
 		};
 
 		handle.print(format!("Loading all of the songs in [{name}]."));
-		let mut files: Vec<(Box<str>, File)> = song
+		let mut files: Vec<Stream> = song
 			.into_iter()
 			.filter_map(|Song { name, file }|
 				{
 					handle.print(format!("Loading the audio contents of [{name}]."));
-					match File::open(fmt_path(file)) {
-						Ok(contents) => Some((name, contents)),
+					let formatted = fmt_path(file);
+					let contents = match File::open(&formatted) {
+						Ok(contents) => contents,
 						Err(why) => {
 							handle.print(format!("{LINE}An error occured whilst attempting to load the audio contents of [{name}]; '{ENBOLD}{why}{DISBOLD}'"));
-							None
+							None?
 						},
-					}
+					};
+					let duration = match read_from_path(formatted) {
+						Ok(tagged) => tagged
+							.properties()
+							.duration(),
+						Err(why) => {
+							handle.print(format!("{LINE}An error occured whilst attempting to aquire the audio properties of [{name}]; '{ENBOLD}{why}{DISBOLD}'"));
+							None?
+						},
+					};
+					Some(Stream { name, file: contents, time: duration })
 				}
 			)
 			.collect();
 		handle.print('\0');
 
-		let mut generator = fastrand::Rng::new();
-
-		handle.print("Determining the output device.");
-		let handles = match OutputStream::try_default() {
-			Ok(handles) => handles,
-			Err(why) => {
-				handle.print(format!("{LINE}A fatal error occured whilst attempting to determine the default audio output device; '{ENBOLD}{why}{DISBOLD}'"));
-				return
-			},
-		};
-
 		handle.print(format!("Playing back all of the songs in [{name}]."));
-		while !files.is_empty() {
-			let (name, contents) = files.remove(generator.usize(0..files.len()));
+		'playback: while !files.is_empty() {
+			let Stream { name, file, time } = files.remove(generator.usize(0..files.len()));
 			match handles
 				.1
-				.play_once(contents)
+				.play_once(file)
 			{
 				Ok(playback) => {
 					handle.print(format!("Playing back the audio contents of [{name}]."));
-					playback.sleep_until_end();
+					let measure = Instant::now();
+					while measure.elapsed() < time {
+						match receiver.try_recv() {
+							Ok(Signal::ManualExit) => {
+								drop(playback);
+								break 'playback
+							},
+							Ok(Signal::TogglePlayback) => if playback.is_paused() { playback.play() } else { playback.pause() },
+							Ok(Signal::SkipPlayback) => {
+								playback.stop();
+								break
+							},
+							Err(TryRecvError::Empty) => continue,
+							Err(why) => {
+								handle.print(format!("{LINE}A fatal error occured whilst attempting to receive a signal from the playback control thread; '{ENBOLD}{why}{DISBOLD}'"));
+								break 'playback
+							},
+						}
+					}
 				},
 				Err(why) => {
 					handle.print(format!("{LINE}An error occured whilst attempting to playback [{name}] from the default audio output device; '{ENBOLD}{why}{DISBOLD}'"));
@@ -136,6 +190,9 @@ fn main() {
 				},
 			}
 		}
+
 	}
+	if let Err(why) = exit_sender.send(0) { handle.print(format!("{LINE}An error occured whilst attempting to send the exit signal to the playback control thread; '{ENBOLD}{why}{DISBOLD}'")) };
+	let _ = playback_control.join();
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
