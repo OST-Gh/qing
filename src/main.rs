76f0dc2cc91e0074f28dc75f6ef066ceb1b4d5a8
OST-Gh <108372693+OST-Gh@users.noreply.github.com>
2023-07-10 14:35:27 +0200

    no multithread

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::{
 	fs::{ self, File },
 	path::{ PathBuf, MAIN_SEPARATOR_STR },
 	time::{ Duration, Instant },
-	thread::spawn,
 	io::{ BufReader, Seek },
 	env::{ var, args },
 };
@@ -21,7 +20,6 @@ use crossterm::{
 		KeyCode,
 	},
 };
-use crossbeam_channel::{ unbounded, RecvTimeoutError };
 use lofty::{ read_from_path, AudioFile };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const FOURTH_SECOND: Duration = Duration::from_millis(250);
@@ -38,14 +36,6 @@ struct Songlist {
 struct Song {
 	name: Box<str>,
 	file: Box<str>,
-}
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-enum Signal {
-	ManualExit, // signal sent by pb_ctl to main for indication of the user manually exiting
-	SkipPlaylist,
-	SkipNext,
-	SkipBack,
-	TogglePlayback,
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 macro_rules! log {
@@ -70,8 +60,8 @@ fn fmt_path(text: impl AsRef<str>) -> PathBuf {
 		.split(MAIN_SEPARATOR_STR)
 		.filter_map(|part|
 			{
-				if part == "~" { return var("HOME").ok() };
-				if part.starts_with('$') { return var(&part[1..]).ok() };
+				if part == "~" { return var("HOME").ok() }
+				if part.starts_with('$') { return var(&part[1..]).ok() }
 				Some(String::from(part))
 			}
 		)
@@ -84,47 +74,10 @@ fn main() {
 	let mut files = args()
 		.skip(1) // skips the executable path (e.g.: //usr/local/bin/{bin-name})
 		.peekable();
-	if files.peek() == None { println!("Requires at least one playlist.toml file."); return }
+	if files.peek() == None { return println!("Requires at least one playlist.toml file.") }
 
-	if let Err(why) = enable_raw_mode() { log!(err: "enable the raw mode of the current terminal" => why); return }
+	if let Err(why) = enable_raw_mode() { return log!(err: "enable the raw mode of the current terminal" => why) }
 	let mut generator = fastrand::Rng::new();
-
-	log!(info: "Spinning up the playback control thread.");
-	let (sender, receiver) = unbounded();
-	let (exit_sender, exit_receiver) = unbounded();
-	let playback_control = spawn(
-		move || 'input: loop {
-			match exit_receiver.recv_timeout(FOURTH_SECOND) { // TODO: minimise possible sources of idle-wake-ups
-				Ok(_) => break,
-				Err(RecvTimeoutError::Timeout) => {
-					let event = 'read: {
-						match event::poll(FOURTH_SECOND) {
-							Ok(truth) => if truth { break 'read event::read() },
-							Err(why) => log!(err: "poll an event from the current terminal" => why),
-						};
-						continue 'input
-					};
-					let signal = 'signal: {
-						match event {
-							Ok(Event::Key(KeyEvent { code: KeyCode::Char(key), .. })) => break 'signal match key {
-								'q' | 'c' => Signal::ManualExit,
-								'/' | 'h' => Signal::SkipPlaylist,
-								'.' | 'l' => Signal::SkipNext,
-								',' | 'j' => Signal::SkipBack,
-								' ' | 'k' => Signal::TogglePlayback,
-								_ => continue 'input,
-							},
-							Err(why) => log!(err: "read an event from the current terminal" => why),
-							_ => { },
-						}
-						continue 'input
-					};
-					if let Err(why) = sender.send(signal) { log!(err: "send a signal to the playback" => why) }
-				},
-				Err(why) => log!(err: "receive a signal from the main thread" => why),
-			};
-		}
-	);
 
 	log!(info: "Determining the output device.");
 	let handles = match rodio::OutputStream::try_default() {
@@ -137,14 +90,13 @@ fn main() {
 	};
 
 	'queue: for path in files {
-
 		log!(info[path]: "\n\nLoading and parsing data from [{path}].");
 		let Songlist { mut song, name } = 'load: {
 			match fs::read_to_string(&path).map(|contents| toml::from_str(&contents)) {
 				Ok(Ok(playlist)) => break 'load playlist,
 				Ok(Err(why)) => log!(err[path]: "parse the contents of [{path}]" => why),
 				Err(why) => log!(err[path]: "load the contents of [{path}]" => why),
-			};
+			}
 			continue 'queue
 		};
 
@@ -189,46 +141,58 @@ fn main() {
 		let mut index = 0;
 
 		'playlist: while index < length {
-			'playback: {
-				let (name, duration) = unsafe { song.get_unchecked(index) };
-				match handles
-					.1
-					.play_once(unsafe { FILES.get_unchecked_mut(index) })
-				{
-					Ok(playback) => {
-						log!(info[name]: "Playing back the audio contents of [{name}].");
-						let mut elapsed = Duration::ZERO;
-						while &elapsed <= duration {
-							let now = Instant::now();
-							let time = match receiver.recv_deadline(now + FOURTH_SECOND) {
-								Ok(Signal::ManualExit) => break 'queue,
-								Ok(Signal::SkipPlaylist) => break 'playlist,
-								Ok(Signal::SkipNext) => break,
-								Ok(Signal::SkipBack) => break 'playback index -= (index > 0) as usize,
-								Ok(Signal::TogglePlayback) => {
-									if playback.is_paused() { playback.play() } else { playback.pause() }
-									now.elapsed()
-								},
-								Err(RecvTimeoutError::Timeout) => FOURTH_SECOND,
-								Err(why) => {
-									log!(err: "receive a signal from the playback control thread" => why);
-									break 'queue
-								},
-							};
-							if !playback.is_paused() { elapsed += time }
-						}
-					},
-					Err(why) => log!(err[name]: "playback [{name}] from the default audio output device" => why),
-				}
-				if let Err(why) = unsafe { FILES.get_unchecked_mut(index) }.rewind() { log!(err[name]: "reset the player position inside of [{name}]" => why) }
-				index += 1;
+			let (name, duration) = unsafe { song.get_unchecked(index) };
+			match handles
+				.1
+				.play_once(unsafe { FILES.get_unchecked_mut(index) })
+			{
+				Ok(playback) => {
+					log!(info[name]: "Playing back the audio contents of [{name}].");
+					let mut elapsed = Duration::ZERO;
+					'play: while &elapsed <= duration {
+						let now = Instant::now();
+						let event = 'read: {
+							match event::poll(FOURTH_SECOND) {
+								Ok(truth) => if truth { break 'read event::read() },
+								Err(why) => log!(err: "poll an event from the current terminal" => why),
+							}
+							continue 'play
+						};
+						let time = match event {
+							Ok(Event::Key(KeyEvent { code: KeyCode::Char(key), .. })) => {
+								match key {
+									'q' | 'c' => break 'queue,
+									'/' | 'h' => break 'playlist,
+									',' | 'j' | '.' | 'l' => if let Err(why) = unsafe { FILES.get_unchecked_mut(index) }.rewind() { log!(err[name]: "reset the player position inside of [{name}]" => why) } else {
+										match key {
+											',' | 'j' => {
+												index -= (index > 0) as usize;
+												continue 'playlist
+											},
+											'.' | 'l' | _ => break 'play
+										}
+									},
+									' ' | 'k' => if playback.is_paused() { playback.play() } else { playback.pause() },
+									_ => { },
+								}
+								now.elapsed()
+							},
+							Err(why) => {
+								log!(err: "read an event from the current terminal" => why);
+								break 'queue
+							},
+							_ => FOURTH_SECOND,
+						};
+						if !playback.is_paused() { elapsed += time }
+					}
+				},
+				Err(why) => log!(err[name]: "playback [{name}] from the default audio output device" => why),
 			}
+			index += 1
 		}
-		unsafe { FILES.clear() };
+		unsafe { FILES.clear() }
 	}
 
-	if let Err(why) = exit_sender.send(0) { log!(err: "send the exit signal to the playback control thread" => why) }
-	let _ = playback_control.join(); // won't (probably) error.
 	if let Err(why) = disable_raw_mode() { log!(err: "disable the raw mode of the current terminal" => why) }
 	print!("\r\x1b[0m\0")
 }
