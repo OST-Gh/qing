9b537c81999fe02c27f6fe47c2a2038768eca5b7
OST-Gh <108372693+OST-Gh@users.noreply.github.com>
2023-07-22 15:41:41 +0200

    skip back and time at playlist level

--- README.md
+++ README.md
@@ -22,17 +22,10 @@ time = -1 # optional setting for repeating a song n times. if the number is nega
 ```toml
 c = "exit the program when in active playback"
 n = "skip one playlist forwards"
+b = "skip one playlist backwards"
 l = "skip one track forwards"
 j = "skip one track backwards"
 k = "pause or start the playback"
 ```
 
 ###### NOTE: all files are loaded upon startup, meaning that there's a (os set) hard cap on playlist length.
-
-### Other:
-> The current implementation of the controls is causing a lot of idle-wake-ups on MacOS which i'd like to minimise.
-> Another problem is that whenever this program runs, it sets the current terminal's mode into raw which only gets turned off upon exiting.
-> What i am trying to say is that when the program doesn't exit naturally it'll cause some problems for the current terminal.
->
-> Another thing i've recently noticed that it is impacting the cpu quite the bit and i'd also like to minimize that.
-> If anyone knows how to do so, please do open an issua or a pull-request on quing's github.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//! I don't know why, but i'm making Docs for this
+//! I don't know why, but i am making Docs for this
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 use std::{
 	thread::{ JoinHandle, Builder },
@@ -35,7 +35,7 @@ const FOURTH_SECOND: Duration = Duration::from_millis(250);
 /// Constant used for rewinding.
 const SECOND: Duration = Duration::from_secs(1);
 
-/// Interthread communication channel disconnected.
+/// Inter-thread communication channel disconnected.
 const DISCONNECTED: &'static str = "DISCONNECTED CHANNEL";
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Global audio stream data.
@@ -46,9 +46,12 @@ static mut FILES: Vec<BufReader<File>> = Vec::new();
 struct Songlist {
 	name: Option<Box<str>>,
 	song: Vec<Song>,
+        :close
+
 }
 
 #[derive(Deserialize)]
+#[derive(Clone)]
 /// Track
 struct Song {
 	name: Option<Box<str>>,
@@ -64,20 +67,21 @@ struct State {
 	signal: Receiver<Signal>,
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// High level control signal repressentation
+/// High level control signal representation
 enum Signal {
-	ManualExit, // signal sent by pb_ctl to main for indication of the user manually exiting
-	SkipPlaylist,
+	ManualExit,
+	SkipNextPlaylist,
+	SkipBackPlaylist,
 	SkipNext,
 	SkipBack,
 	TogglePlayback,
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// Macro for general interaction with Stdout.
+/// Macro for general interaction with Standard-out.
 macro_rules! log {
 	(err$([$($visible: ident)+])?: $message: literal => $($why: ident)+ $(; $($retaliation: tt)+)?) => {
 		{
-			print!(concat!("\r\x1b[38;2;254;205;33m\x1b[4mAn error occured whilst attempting to ", $message, ';') $(, $($visible = $visible),+)?);
+			print!(concat!("\r\x1b[38;2;254;205;33m\x1b[4mAn error occurred whilst attempting to ", $message, ';') $(, $($visible = $visible),+)?);
 			$(print!(" '\x1b[1m{}\x1b[22m'", format!("{}", $why).replace('\n', "\r\n"));)+
 			println!("\x1b[24m\0");
 			$($($retaliation)+)?
@@ -112,7 +116,7 @@ fn fmt_path(text: impl AsRef<str>) -> PathBuf {
 			)
 			.collect::<Vec<String>>()
 			.join(MAIN_SEPARATOR_STR)
-	)// esc(\) parsing
+	)
 		.canonicalize()
 		.unwrap_or_else(|why| log!(err: "canonicalise a path" => why; PathBuf::new()))
 }
@@ -142,8 +146,12 @@ fn panic_handle(info: &panic::PanicInfo) {
 			.get(1)
 			.unwrap_or(&"NO_DISPLAYABLE_INFORMATION")
 			.replace('\n', "\r\n");
-		println!("\r\x1b[38;2;254;205;33m\x1b[4mAn error occured whilst attempting to {message}; '\x1b[1m{reason}\x1b[22m'\x1b[24m\0")
+		println!("\r\x1b[38;2;254;205;33m\x1b[4mAn error occurred whilst attempting to {message}; '\x1b[1m{reason}\x1b[22m'\x1b[24m\0")
 	};
+}
+
+fn next(time: &mut isize, index: &mut usize) {
+	if *time == 0 { *index += 1 } else { *time -= 1 }
 }
 
 fn main() {
@@ -161,17 +169,25 @@ fn main() {
 
 	let mut generator = fastrand::Rng::new();
 	let init = OnceCell::new(); // expensive operation only executed if no err.
-
-	'queue: for path in files {
-
-		log!(info[path]: "Loading and parsing data from [{path}].");
-		let Songlist { mut song, name } = match fs::read_to_string(fmt_path(&path)).map(|contents| toml::from_str(&contents)) {
-			Ok(Ok(playlist)) => playlist,
-			Ok(Err(why)) => log!(err[path]: "parse the contents of [{path}]" => why; continue 'queue),
-			Err(why) => log!(err[path]: "load the contents of [{path}]" => why; continue 'queue),
-		};
-		let name = name.unwrap_or_default();
-
+	let mut lists: Vec<(Vec<Song>, Box<str>, isize)> = files
+		.filter_map(|path|
+			{
+				log!(info[path]: "Loading and parsing data from [{path}].");
+				let Songlist { song, name, time } = match fs::read_to_string(fmt_path(&path)).map(|contents| toml::from_str(&contents)) {
+					Ok(Ok(playlist)) => playlist,
+					Ok(Err(why)) => log!(err[path]: "parse the contents of [{path}]" => why; None?),
+					Err(why) => log!(err[path]: "load the contents of [{path}]" => why; None?),
+				};
+				Some((song, name.unwrap_or_default(), time.unwrap_or_default()))
+			}
+		)
+		.collect();
+	let length = lists.len();
+	let mut list_index = 0;
+	'queue: while list_index < length {
+		let old_list_index = list_index;
+		let (song, name, list_time) = unsafe { lists.get_unchecked_mut(old_list_index) };
+		let mut song = song.clone();
 		let mut song: Vec<(Box<str>, Duration, isize)> = {
 
 			log!(info[name]: "Shuffling all of the songs in [{name}].");
@@ -204,45 +220,58 @@ fn main() {
 
 
 		let length = song.len();
-		let mut index = 0;
+		let mut song_index = 0;
 
-		'playlist: while index < length && !control.is_finished() {
-			let old = index; // (sort of) proxy to index (used because of rewind code)
-			// unless something is very wrong with the index (old), this will not error.
-			let (name, duration, time) = unsafe { song.get_unchecked_mut(old) };
-			match output
-				.1
-				.play_once(unsafe { FILES.get_unchecked_mut(old) })
-			{
-				Ok(playback) => 'playback: {
-					log!(info[name]: "Playing back the audio contents of [{name}].");
-					let mut elapsed = Duration::ZERO;
-					while &elapsed <= duration {
-						let now = Instant::now();
-						let paused = playback.is_paused();
-						elapsed += match signal.recv_deadline(now + FOURTH_SECOND) {
-							Err(RecvTimeoutError::Timeout) => if paused { continue } else { FOURTH_SECOND },
+		'list_playback: { // i hate this
+			'playlist: while song_index < length && !control.is_finished() {
+				let old_song_index = song_index; // (sort of) proxy to index (used because of rewind code)
+				// unless something is very wrong with the index (old), this will not error.
+				let (name, duration, song_time) = unsafe { song.get_unchecked_mut(old_song_index) };
+				match output
+					.1
+					.play_once(unsafe { FILES.get_unchecked_mut(old_song_index) })
+				{
+					Ok(playback) => 'song_playback: {
+						log!(info[name]: "Playing back the audio contents of [{name}].");
+						let mut elapsed = Duration::ZERO;
+						while &elapsed <= duration {
+							let now = Instant::now();
+							let paused = playback.is_paused();
+							elapsed += match signal.recv_deadline(now + FOURTH_SECOND) {
+								Err(RecvTimeoutError::Timeout) => if paused { continue } else { FOURTH_SECOND },
 
-							Ok(Signal::ManualExit) => break 'queue,
-							Ok(Signal::SkipPlaylist) => break 'playlist,
-							Ok(Signal::SkipNext) => {
-								*time = 0;
-								break
-							},
-							Ok(Signal::SkipBack) => break 'playback index -= (old > 0 && elapsed <= SECOND) as usize,
-							Ok(Signal::TogglePlayback) => {
-								if paused { playback.play() } else { playback.pause() }
-								now.elapsed()
-							},
+								Ok(Signal::ManualExit) => break 'queue,
 
-							Err(RecvTimeoutError::Disconnected) => break 'queue, // chain reaction will follow
-						};
-					}
-					if *time == 0 { index += 1 } else { *time -= 1 }
-				},
-				Err(why) => log!(err[name]: "playback [{name}] from the default audio output device" => why; break 'queue), // assume error will occur on the other tracks too
-			};
-			if let Err(why) = unsafe { FILES.get_unchecked_mut(old) }.rewind() { log!(err[name]: "reset the player position inside of [{name}]" => why) }
+								Ok(Signal::SkipNextPlaylist) => {
+									*list_time = 0;
+									break 'playlist
+								},
+								Ok(Signal::SkipBackPlaylist) => {
+									list_index -= (old_list_index > 0 && elapsed <= SECOND) as usize;
+									break 'list_playback
+								},
+
+								Ok(Signal::SkipNext) => {
+									*song_time = 0;
+									break
+								},
+								Ok(Signal::SkipBack) => break 'song_playback song_index -= (old_song_index > 0 && elapsed <= SECOND) as usize,
+
+								Ok(Signal::TogglePlayback) => {
+									if paused { playback.play() } else { playback.pause() }
+									now.elapsed()
+								},
+
+								Err(RecvTimeoutError::Disconnected) => break 'queue, // chain reaction will follow
+							};
+						}
+						next(song_time, &mut song_index)
+					},
+					Err(why) => log!(err[name]: "playback [{name}] from the default audio output device" => why; break 'queue), // assume error will occur on the other tracks too
+				};
+				if let Err(why) = unsafe { FILES.get_unchecked_mut(old_song_index) }.rewind() { log!(err[name]: "reset the player position inside of [{name}]" => why) }
+			}
+			next(list_time, &mut list_index);
 		}
 		unsafe { FILES.clear() }
 		print!("\r\n\n\0");
@@ -284,7 +313,8 @@ impl State {
 								if let Err(why) = sender.send(Signal::ManualExit) { log!(err: "send a signal to the playback" => why) }
 								return
 							},
-							Ok(Event::Key(KeyEvent { code: KeyCode::Char('n' | 'N'), .. })) => Signal::SkipPlaylist,
+							Ok(Event::Key(KeyEvent { code: KeyCode::Char('n' | 'N'), .. })) => Signal::SkipNextPlaylist,
+							Ok(Event::Key(KeyEvent { code: KeyCode::Char('b' | 'B'), .. })) => Signal::SkipBackPlaylist,
 							Ok(Event::Key(KeyEvent { code: KeyCode::Char('l' | 'L'), .. })) => Signal::SkipNext,
 							Ok(Event::Key(KeyEvent { code: KeyCode::Char('j' | 'J'), .. })) => Signal::SkipBack,
 							Ok(Event::Key(KeyEvent { code: KeyCode::Char('k' | 'K'), .. })) => Signal::TogglePlayback,
@@ -314,7 +344,7 @@ impl State {
 		if !control.is_finished() {
 			let _ = exit.send(0); // error might occur between check, manual shutdown on control side, and exit signal sending
 			// not really an error.
-		} // assume that there's no error here. if there is, then the thread either finished(/panicked) 
+		} // assume that there's no error here. If there is, then the thread either finished(/panicked) 
 		if let Err(why) = control.join() {
 			let why = panic_payload(&why);
 			log!(err: "clean up the playback control thread" => why)
